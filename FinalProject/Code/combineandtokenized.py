d9a6b7a5bef3e5c8565cb89ba526cead8db6fce7
hejiaxu7146 <35708538+hejiaxu7146@users.noreply.github.com>
2018-12-03 16:34:50 -0500

    Add files via upload

---
+++ FinalProject/Code/combineandtokenized.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 16:08:52 2018
+
+@author: hejia
+"""
+
+import pandas as pd
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+import re
+stopword = set(stopwords.words('english'))
+porter = PorterStemmer()
+
+def tokenize(text):
+    letters_only = re.sub("[^a-zA-Z]",  # Search for all non-letters
+                          " ",          # Replace all non-letters with spaces
+                          str(text))     # Column and row to search
+    words = word_tokenize(letters_only) #split words
+    words = [w.lower() for w in words if w.isalpha()] #get rid of punctuation
+    words =[w for w in words if  not w in stopword]
+    stemmed = [porter.stem(w) for w in words]
+    stemmed = ' '.join(w for w in stemmed)
+    return stemmed
+
+train = pd.read_csv("yelp2017usefultrain.csv")
+test =pd.read_csv("yelp2017usefultest.csv")
+
+trainfinal =  [train, test]
+
+final = pd.concat(trainfinal)
+
+
+final["text"] = final.text.map(tokenize)
+final["text"] = final["text"].astype(str)
+final.to_csv("yelp2017useful.csv", index = False)
